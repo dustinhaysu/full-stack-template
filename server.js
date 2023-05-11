95a8d985a9f320db6afb44d3bd32c7725d44e954
Dustin Hays <hays.dustin.u@gmail.com>
2023-05-10 20:39:52 -0700

    variables, mongo connection

--- server.js
+++ server.js
@@ -5,3 +5,15 @@ const MongoClient = require('mongodb').MongoClient
 require('dotenv').config()
 
 console.log('We\'re cooking with grease baby')
+
+let db,
+    dbConnectionString = process.env.DB_STRING,
+    dbName = 'sample_mflix',
+    collection
+
+MongoClient.connect(dbConnectionString)
+    .then(client => {
+        console.log('Connected to Database')
+        db = client.db(dbName)
+        collection = db.collection('movies')
+    })
