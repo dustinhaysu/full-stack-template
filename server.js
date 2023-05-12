898d12322e51235e4efece6d77ad886d3925cbd1
Dustin Hays <hays.dustin.u@gmail.com>
2023-05-12 12:12:10 -0700

    add middleware

--- server.js
+++ server.js
@@ -17,6 +17,17 @@ MongoClient.connect(dbConnectionString)
         collection = db.collection('movies')
     })
 
+//MIDDLEWARE
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+app.use(cors())
+
+
     app.listen(process.env.PORT || PORT, ()=> {
         console.log(`Server is running on port ${process.env.PORT || PORT}`)
     })
+
+    
